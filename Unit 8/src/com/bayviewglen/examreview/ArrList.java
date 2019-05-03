dc958dee30d9627ddbaa0eed9fd760566dc5db87
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-03 11:41:22 -0400

    Studying for AP Exam

---
+++ Unit 8/src/com/bayviewglen/examreview/ArrList.java
@@ -0,0 +1,19 @@
+package com.bayviewglen.examreview;
+
+import java.util.ArrayList;
+
+public class ArrList {
+
+	public static void main(String[] args) {
+		ArrayList<String> words = new ArrayList<String>(); 
+		words.add("hard");
+		words.add("work"); 
+		words.add("pays");
+		words.add("off"); 
+		
+		
+	}
+
+
+
+}
