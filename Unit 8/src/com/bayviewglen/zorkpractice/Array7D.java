79436522260ee29fbc1d8e95de4e8182c253c785
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2019-05-14 14:22:14 -0400

    7-dimensional array!!

---
+++ Unit 8/src/com/bayviewglen/zorkpractice/Array7D.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.zorkpractice;
+
+public class Array7D {
+
+	public static void main(String[] args) {
+		int[][][][][][][][] array7D = new int[5][6][7][5][4][5][5][9]; 
+	}
+
+}
